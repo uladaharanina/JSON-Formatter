01e06b2351c51a428bf011dabb7b97cdad12fb73
RoryEiffe <roryeiffe8@gmail.com>
2025-03-04 22:04:15 -0500

    refactored form to use drop-down for activity type, also added check so duplicate activities can't be added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,11 @@ import './App.css'
 import React, {MouseEventHandler} from 'react';
 import { AppProps } from './types';
 import UnitDisplay from './components/UnitDisplay';
-import AddActivity from './components/AddActivity';
 
 function App({ onClick, text }:AppProps) {
 
   return (
-    <>
-      <></>
-      <UnitDisplay />
-      
-    </>
+    <UnitDisplay />
   )
 }
 
--- src/components/UnitDisplay.tsx
+++ src/components/UnitDisplay.tsx
@@ -7,84 +7,119 @@ import SAMPLE_DATA from '../../sample-jsons/sample-navigation.json';
 import AddActivity from "./AddActivity";
 
 
+const checkActivityExists = (activityToAdd: Activity, activities: Activity[]) => {
+  let found = false;
+  for (const activity of activities) {
+    if (activity.activityName === activityToAdd.activityName && activity.activityType === activityToAdd.activityType) {
+      found = true;
+      break;
+    }
+  }
+  return found;
+}
+
+
 function UnitDisplay() {
-
-
-  const [data, setData] = useState<Unit>();
-
-  const [currentEdit, setCurrentEdit] = useState<HierarchyItem>({title: '', id: ''});
+  const [unitTaxonomy, setUnitTaxonomy] = useState<Unit>(SAMPLE_DATA);
+  const [currentEdit, setCurrentEdit] = useState<HierarchyItem>({ title: '', id: '' });
 
   useEffect(() => {
     // initialize empty activity arrays for all unit/modules/topics:
-    const dataCopy:Unit = SAMPLE_DATA;
+    const dataCopy: Unit = SAMPLE_DATA;
     dataCopy.activities = [];
-    for(let i = 0; i < dataCopy.modules.length; i ++) {
+    for (let i = 0; i < dataCopy.modules.length; i++) {
       dataCopy.modules[i].activities = [];
-      for(let j = 0; j < dataCopy.modules[i].topics.length; j ++) {
+      for (let j = 0; j < dataCopy.modules[i].topics.length; j++) {
         dataCopy.modules[i].topics[j].activities = [];
       }
     }
-    setData(dataCopy);
-  }, [data])
+  }, [unitTaxonomy])
 
-  const addActivity = (activityDetails: Activity, hierarchyType: HierarchyType, id: string) => {
-    console.log("Now, do something with this:");
-    console.log(activityDetails);
-    console.log(id);
-    console.log(hierarchyType);
-
-    const dataCopy = data;
-    if(!dataCopy) return;
-
-    switch(hierarchyType) {
+  const addActivityHandler = (activityDetails: Activity, hierarchyType: HierarchyType, id: string) => {
+    const taxonomy = unitTaxonomy;
+    switch (hierarchyType) {
       case HierarchyType.UNIT:
-        dataCopy?.activities?.push(activityDetails);
+        setUnitTaxonomy(addActivityToUnit(taxonomy, activityDetails, id));
         break;
       case HierarchyType.MODULE:
-        for(let i = 0; i < dataCopy?.modules.length; i ++) {
-          if(dataCopy?.modules[i].id === id) {
-            dataCopy?.modules[i].activities?.push(activityDetails);
-            break;
-          }
-        }
+        setUnitTaxonomy(addActivityToModule(taxonomy, activityDetails, id));
         break;
       case HierarchyType.TOPIC:
-        for(let i = 0; i < dataCopy?.modules.length; i ++) {
-          for(let j = 0; j < (dataCopy?.modules[i].topics?.length || 0); j ++) {
-            if(dataCopy.modules[i].topics[j].id === id) {
-              dataCopy.modules[i].topics[j].activities?.push(activityDetails);
-            }
-          }
-        }
+        setUnitTaxonomy(addActivityToTopic(taxonomy, activityDetails, id));
         break;
     }
-
-    setData(dataCopy);
-    console.log(dataCopy);
-
   }
 
+  const addActivityToUnit = (taxonomy: Unit, activityDetails: Activity, id: string) => {
+    let activities = taxonomy.activities;
+    if (taxonomy.id === id) {
+      if (!checkActivityExists(activityDetails, activities!)) taxonomy.activities?.push(activityDetails);
+      else alert('Duplicate Activity Name + Type')
+    }
+    return taxonomy;
+  }
 
+  const addActivityToModule = (taxonomy: Unit, activityDetails: Activity, id: string) => {
+    for (let i = 0; i < taxonomy?.modules.length; i++) {
+      if (taxonomy?.modules[i].id === id) {
+        let activities = taxonomy.modules[i].activities;
+        if (!checkActivityExists(activityDetails, activities!)) taxonomy.modules[i].activities?.push(activityDetails);
+        else alert('Duplicate Activity Name + Type');
+        break;
+      }
+    }
+    return taxonomy;
+  }
+
+  const addActivityToTopic = (taxonomy: Unit, activityDetails: Activity, id: string) => {
+    for (let i = 0; i < taxonomy?.modules.length; i++) {
+      for (let j = 0; j < (taxonomy?.modules[i].topics?.length || 0); j++) {
+        if (taxonomy.modules[i].topics[j].id === id) {
+          let activities = taxonomy.modules[i].topics[j].activities;
+          if (!checkActivityExists(activityDetails, activities!)) taxonomy.modules[i].topics[j].activities?.push(activityDetails);
+          else alert('Duplicate Activity Name + Type');
+          return taxonomy;
+        }
+      }
+    }
+    return taxonomy;
+  }
 
   return (
 
     <div className='container'>
       <div>
-        {data &&
+        {unitTaxonomy &&
           <div className='unit'>
-            <h1 className='unit-title'>{data.title} (Unit)</h1>
-            <h2 className='unit-description'>{data.description}</h2>
-            <button onClick={() => setCurrentEdit({hierarchyType:HierarchyType.UNIT, title: data.title, id: data.id})}>Add Activity (Unit Level)</button>
+            <h1 className='unit-title'>{unitTaxonomy.title} (Unit)</h1>
+            <h2 className='unit-description'>{unitTaxonomy.description}</h2>
+            <ul>
+              {unitTaxonomy.activities?.map((activity: Activity) =>
+                <li key={activity.activityName + activity.activityType}>{activity.activityName}</li>
+              )}
+            </ul>
+            <button onClick={() => setCurrentEdit({ hierarchyType: HierarchyType.UNIT, title: unitTaxonomy.title, id: unitTaxonomy.id })}>Add Activity (Unit Level)</button>
 
-            {data.modules.map(module =>
+            {unitTaxonomy.modules.map(module =>
               <div className='module' key={module.id}>
                 <h3 className='module-title'>{module.title} (Module)</h3>
                 <h3 className='module-description'>{module.description}</h3>
-                <button onClick={() => setCurrentEdit({hierarchyType:HierarchyType.MODULE, title: module.title, id: module.id})}>Add Activity (Module Level)</button>
+                <ul>
+                  {module.activities?.map((activity: Activity) =>
+                    <li key={activity.activityName + activity.activityType}>{activity.activityName}</li>
+                  )}
+                </ul>
+                <button onClick={() => setCurrentEdit({ hierarchyType: HierarchyType.MODULE, title: module.title, id: module.id })}>Add Activity (Module Level)</button>
+
                 {module.topics.map(topic =>
                   <div className='topic' key={topic.id}>
                     <p className='topic-title'>{topic.title} (Topic)</p>
-                    <button onClick={() => setCurrentEdit({hierarchyType:HierarchyType.TOPIC, title: topic.title, id: topic.id})}>Add Activity (Topic Level)</button>
+                    <ul>
+                      {topic.activities?.map((activity: Activity) =>
+                        <li key={activity.activityName + activity.activityType}>{activity.activityName}</li>
+                      )}
+                    </ul>
+                    <button onClick={() => setCurrentEdit({ hierarchyType: HierarchyType.TOPIC, title: topic.title, id: topic.id })}>Add Activity (Topic Level)</button>
                   </div>
                 )}
               </div>
@@ -95,13 +130,10 @@ function UnitDisplay() {
       </div>
 
       <div className="add-activity">
-        <AddActivity hierarchyItem={currentEdit} addActivityFunc={addActivity}/>
+        <AddActivity hierarchyItem={currentEdit} addActivityFunc={addActivityHandler} />
       </div>
 
     </div>
-
-
-
   )
 
 }
