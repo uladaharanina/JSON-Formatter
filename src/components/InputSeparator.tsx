f3b5b49fda63a2b50f5d4eed814b8c51b6627a51
uladaharanina <uladaharanina@gmail.com>
2025-03-17 16:56:57 -0400

    fix UI display for exit criteria, skills and tags

--- src/components/InputSeparator.tsx
+++ src/components/InputSeparator.tsx
@@ -13,7 +13,7 @@ export const InputSeparator: React.FC<Props>  = ({insertNewData, removeData}) =>
     const [currentInputList, setCurrentInputList] = useState<string[]>([]);
     const [currentInput, setCurrentInput] = useState<string>();
 
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event:any) => {
         if(event.key === 'Enter' && currentInput != null && currentInput != undefined){
             setCurrentInputList(prev => [...prev, currentInput]);
             insertNewData(currentInput);
@@ -33,13 +33,13 @@ export const InputSeparator: React.FC<Props>  = ({insertNewData, removeData}) =>
                 className="w-full p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-700"
                 value = {currentInput} 
                 onChange={(e) => setCurrentInput(e.target.value)}/>
-            <div className=" my-5 flex">
+            <div className=" my-5 grid-cols-2">
                 {
                     currentInputList && (
                         currentInputList.map((item, index) => (
                             <div key={index} 
-                            className="py-2 px-5 bg-blue-300  text-black mx-1 rounded-md shadow-lg hover:bg-blue-500  cursor-pointer
-                            transition-all duration-300 flex items-center gap-3">
+                            className="py-2 px-5 my-2 bg-blue-300  text-black mx-1 rounded-md shadow-lg hover:bg-blue-500  cursor-pointer
+                            transition-all duration-300 flex items-center gap-3 justify-between">
                                 {item}
                                 <span onClick={() => RemoveInput(index)}><IoMdCloseCircle></IoMdCloseCircle>
                                 </span>
